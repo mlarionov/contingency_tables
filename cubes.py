00307dd3e4bd1f75ad3e4acabd4d6145d385989f
Michael Larionov <6360814+mlarionov@users.noreply.github.com>
2020-12-31 20:01:38 -0600

    Added a method to lookup the position based on the indexes in multidimenssional array

--- cubes.py
+++ cubes.py
@@ -7,14 +7,14 @@ class Cube(object):
     def __init__(self, shape: tuple, flat):
         self.flat = flat
         self.shape = shape
-        self.validate_shape(shape, flat)
+        self._validate_shape(shape, flat)
 
     @staticmethod
     def compute_dimension(shape: tuple):
         return reduce(operator.mul, shape)
 
     @staticmethod
-    def validate_shape(shape, flat):
+    def _validate_shape(shape, flat):
         # Validate if only the first dimension is non-trivial
         if len(flat.shape) > 0:
             for dim_slice in flat.shape[1:]:
@@ -22,3 +22,13 @@ class Cube(object):
                     return False
         return Cube.compute_dimension(shape) == flat.shape[0]
 
+    @staticmethod
+    def _lookup_position(row, shape):
+        item = list(row)
+        assert len(item) == len(shape)
+        item = [element % dimension for element, dimension in zip(item, shape)]
+        multipliers = [1] * len(shape)
+        for i in range(len(shape)-1,0,-1):
+            multipliers[i-1] = multipliers[i] * shape[i]
+        multiplied = [element * multiplier for element, multiplier in zip(item, multipliers)]
+        return reduce(operator.add, multiplied)
--- tests/cube_test.py
+++ tests/cube_test.py
@@ -5,9 +5,15 @@ from cubes import Cube
 
 class MyTestCase(unittest.TestCase):
     def test_validate_shape(self):
-        self.assertTrue(Cube.validate_shape((2, 3, 4), np.zeros(24)))
-        self.assertFalse(Cube.validate_shape((2, 3, 4), np.zeros(25)))
-        self.assertFalse(Cube.validate_shape((2, 3, 4), np.zeros((24,2))))
+        self.assertTrue(Cube._validate_shape((2, 3, 4), np.zeros(24)))
+        self.assertFalse(Cube._validate_shape((2, 3, 4), np.zeros(25)))
+        self.assertFalse(Cube._validate_shape((2, 3, 4), np.zeros((24, 2))))
+
+    def test_lookup_position(self):
+        self.assertEqual(3, Cube._lookup_position((1, 1), (2, 2)))
+        self.assertEqual(0, Cube._lookup_position((0, 0), (2, 3)))
+        self.assertEqual(36, Cube._lookup_position((1, 3, 1), (2, 4, 5)))
+        self.assertEqual(3, Cube._lookup_position((-1,-1), (2, 2)))
 
 
 if __name__ == '__main__':
