8a04c41115a1605f27c5e769c1746ace61189206
Michael Larionov <6360814+mlarionov@users.noreply.github.com>
2021-01-02 18:46:41 -0600

    Implemented reduce_sum function

--- cubes.py
+++ cubes.py
@@ -1,5 +1,7 @@
 import operator
 from functools import reduce
+import numpy as np
+import itertools
 
 
 class Cube(object):
@@ -8,6 +10,9 @@ class Cube(object):
         self.flat = flat
         self.shape = shape
         self._validate_shape(shape, flat)
+        self.multipliers = [1] * len(shape)
+        for i in range(len(shape) - 1, 0, -1):
+            self.multipliers[i - 1] = self.multipliers[i] * shape[i]
 
     @staticmethod
     def compute_dimension(shape: tuple):
@@ -22,13 +27,48 @@ class Cube(object):
                     return False
         return Cube.compute_dimension(shape) == flat.shape[0]
 
-    @staticmethod
-    def _lookup_position(row, shape):
+    def _lookup_position(self, row):
         item = list(row)
-        assert len(item) == len(shape)
-        item = [element % dimension for element, dimension in zip(item, shape)]
-        multipliers = [1] * len(shape)
-        for i in range(len(shape)-1,0,-1):
-            multipliers[i-1] = multipliers[i] * shape[i]
-        multiplied = [element * multiplier for element, multiplier in zip(item, multipliers)]
+        assert len(item) == len(self.shape)
+        item = [element % dimension for element, dimension in zip(item, self.shape)]
+        multiplied = [element * multiplier for element, multiplier in zip(item, self.multipliers)]
         return reduce(operator.add, multiplied)
+
+    def _reverse_lookup(self, position):
+        items = []
+        for i in range(len(self.multipliers)):
+            items.append(position // self.multipliers[i])
+            position = position % self.multipliers[i]
+        return items
+
+    def shift(self, positions: tuple):
+        """
+        This function shifts along the axes by the positions specified in the positions parameter
+        For example, if we are dealing with 2x2 array, and positions=(1,0),
+        then the indexes of the first dimension will be incremented by one,
+        so the result b_00 = a_10, b_10 = a_00.
+
+        We implement this shift as a square matrix of the dimension of len(flat),
+        so that in the reduce methods it may be easier to do transformation using matrix multiplication
+        :param positions:
+        :return: a square matrix of the same dimension as the flattened data array
+        """
+        assert len(positions) == len(self.shape)
+        flat_dimension = len(self.flat)
+        result = np.zeros((flat_dimension, flat_dimension))
+        for k in range(flat_dimension):
+            items = self._reverse_lookup(k)
+            items = [item + position for item, position in zip(items, positions)]
+            k_prime = self._lookup_position(items) % flat_dimension
+            result[k, k_prime] = 1
+        return result
+
+    def reduce_sum(self, dimensions: tuple, keepdims: bool):
+        assert keepdims, "Currently not supporting keepdims=False"
+        ranges = [range(self.shape[shape_slice]) if shape_slice in dimensions else range(1) for shape_slice in range(len(self.shape))]
+        combinations = itertools.product(*ranges)
+        result = 0
+        for combination in combinations:
+            shift_matrix = self.shift(combination)
+            result = result + shift_matrix @ self.flat
+        return Cube(self.shape, flat=result)
--- tests/cube_test.py
+++ tests/cube_test.py
@@ -10,10 +10,32 @@ class MyTestCase(unittest.TestCase):
         self.assertFalse(Cube._validate_shape((2, 3, 4), np.zeros((24, 2))))
 
     def test_lookup_position(self):
-        self.assertEqual(3, Cube._lookup_position((1, 1), (2, 2)))
-        self.assertEqual(0, Cube._lookup_position((0, 0), (2, 3)))
-        self.assertEqual(36, Cube._lookup_position((1, 3, 1), (2, 4, 5)))
-        self.assertEqual(3, Cube._lookup_position((-1,-1), (2, 2)))
+        self.assertEqual(3, Cube((2, 2), np.zeros(4))._lookup_position((1, 1)))
+        self.assertEqual(0, Cube((2, 3), np.zeros(6))._lookup_position((0, 0)))
+        self.assertEqual(36, Cube((2, 4, 5), np.zeros(40))._lookup_position((1, 3, 1)))
+        self.assertEqual(3, Cube((2, 2), np.zeros(4))._lookup_position((-1, -1)))
+
+    def test_reverse_lookup(self):
+        cube = Cube((2, 4, 5), np.zeros(40))
+        for i in range(40):
+            self.assertEqual(i, cube._lookup_position(cube._reverse_lookup(i)))
+
+    def test_shift(self):
+        cube = Cube((2, 2), np.array([0, 1, 2, 3]))
+        np.testing.assert_equal(np.array([[0., 0., 1., 0.],
+                                          [0., 0., 0., 1.],
+                                          [1., 0., 0., 0.],
+                                          [0., 1., 0., 0.]]), cube.shift((1, 0)))
+
+    def test_reduce_sum(self):
+        flat_data = np.arange(4).astype('float')
+        cube = Cube((2, 2), flat_data)
+        cube0 = cube.reduce_sum((0,), keepdims=True)
+        cube1 = cube.reduce_sum((1,), keepdims=True)
+        cube01 = cube.reduce_sum((0, 1), keepdims=True)
+        np.testing.assert_equal(np.array([2., 4., 2., 4.]), cube0.flat)
+        np.testing.assert_equal(np.array([1., 1., 5., 5.]), cube1.flat)
+        np.testing.assert_equal(np.array([6., 6., 6., 6.]), cube01.flat)
 
 
 if __name__ == '__main__':
